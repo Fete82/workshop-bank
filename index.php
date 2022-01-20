3204bc4414889385eb962090cf8672b37ae203d3
Edvin Lindahl <81903287+Fete82@users.noreply.github.com>
2022-01-20 13:51:44 +0100

    Added VERY IMPORTANT CSS

--- index.php
+++ index.php
@@ -18,7 +18,35 @@ array_push($accounts, $account);
 //Dumps info on top, because clarity.
 //Dump::pre($accounts);
 
-foreach ($accounts as $account) {
-    echo "<p>{$account->getAccountInfo()}</p>";
-    echo "<br>";
-}
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="primary">
+                <div class="logo">
+                    <div><h1>Edbank</h1></div>
+                    <div id="logo-image"><img src="images/swblg.png" width=50px height=auto;></div>
+                </div>
+                <div>
+                    <?php
+                    
+                    foreach ($accounts as $account) {
+                        echo "<p>{$account->getAccountInfo()}</p>";
+                        echo "<br>";
+                    }
+?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
